ffc8c86f95b80c9a39e9ad67eb6eb6b9bf1f0e90
Bhakti <bhaktinirbhaybhatt@gmail.com>
2024-03-17 14:10:46 +0530

    added tailwind css with postcss

---
+++ Client/src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Navbar = () => {
+    return (
+        <div className="h-10 bg-violet-500">
+            <h1>This is the header</h1>
+        </div>
+    )
+}
+export default Navbar
