dd1e9d24bfaca7ea12df232e79ce1bb67a52c3de
Bhakti <bhaktinirbhaybhatt@gmail.com>
2024-03-17 16:50:57 +0530

    navbar Static done, hero img added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Navbar from "./Components/Navbar.jsx"
+import Hero from "./Components/Hero.jsx"
 function App() {
 
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
