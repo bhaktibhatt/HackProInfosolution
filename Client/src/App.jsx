8bcbaf4722909e8ec264e5aaead37039d28633aa
Bhakti <bhaktinirbhaybhatt@gmail.com>
2024-03-19 23:36:59 +0530

    /about,/ ; Endpoint added using react-router-dom for navigation

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import './App.css'
 import Navbar from "./Components/Navbar.jsx"
 import Hero from "./Components/Hero.jsx"
-import Main from "./Components/Main.jsx"
+import Main from "./Components/Home.jsx"
+import About from "./pages/About.jsx"
+import Home from "./Components/Home.jsx"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
 
   return (
-    <div>
-      <Navbar/>
-      <Hero/>
-      <Main/>
-    </div>
+    <Router>
+      <div>
+        <Navbar/>
+       <Routes>
+          <Route  exact path='/' element={<Home/>}></Route>
+          <Route exact path='/about' element={ <About/> }></Route>
+        </Routes> 
+       </div>
+      </Router>
   )
 }
 
--- Client/src/Components/Hero.jsx
+++ Client/src/Components/Hero.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import image from "../assets/image.png"
+
 const Hero = () => {
     return (
-        <div className="relative w-full">
-            <img className="w-full h-3/4" src={ image } alt="" />
-            <button className="absolute top-2/3 left-72 bg-slate-50 px-6 py-4 rounded-2xl text-3xl text-violet-900  hover:drop-shadow-xl hover:">Enroll Now</button>
-        </div>
+        <></>
     )
 }
 export default Hero
