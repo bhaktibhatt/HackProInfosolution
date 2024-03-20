141f6bdac96637cba8eb16217c48345c0512d204
Bhakti <bhaktinirbhaybhatt@gmail.com>
2024-03-20 11:56:20 +0530

    merged main and hero as home and removed extra components form app.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import './App.css'
 import Navbar from "./Components/Navbar.jsx"
-import Hero from "./Components/Hero.jsx"
-import Main from "./Components/Home.jsx"
 import About from "./pages/About.jsx"
 import Home from "./Components/Home.jsx"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
