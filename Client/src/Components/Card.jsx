39f401a083f69d00821a2078d2f75c12df227087
Bhakti <bhaktinirbhaybhatt@gmail.com>
2024-03-25 17:47:25 +0530

    Card Components complete

--- Client/src/Components/Card.jsx
+++ Client/src/Components/Card.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import cardBg from '../assets/card-bg.jpg'
-const card = () => {
+const card = (props) => {
   return (
-    <div className=' w-72 h-3/4 border border-black rounded-3xl'>
+    <div className=' w-72 h-96 border border-black rounded-3xl'>
       <div className='relative'>
-      <img className=' rounded-t-3xl w-72 h-44 ' src={cardBg} alt="" />
-      <h2 className='absolute top-12 left-20 text-white text-2xl'>Course title</h2>
+        <img className=' rounded-t-3xl w-72 h-44 ' src={cardBg} alt="" />
+        <h2 className='w-full absolute top-12 text-white text-3xl text-center'>{props.ctitle}</h2>
       </div>
-      <p className='p-2 mb-0'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, aspernatur? Earum rerum repudiandae dolorum? Reprehenderit sapiente ipsum reiciendis totam quo?</p>
-      <p className='p-2 pt-0 text-right' >learn more &gt;&gt;</p>
+      <p className='p-2 mb-0'>{props.cdes}</p>
+      <p className='font-bold p-2 pt-0 text-right'>learn more &gt;&gt;</p>
     </div>
   )
 }
--- Client/src/Components/Home.jsx
+++ Client/src/Components/Home.jsx
@@ -3,7 +3,7 @@ import Tools from '../assets/Tools.png'
 import image from "../assets/image.png"
 import Marquee from 'react-fast-marquee';
 import Card from "./Card.jsx";
-const Main = () =>{
+const Home = () =>{
     const row1 = [
         "/HackProInfosolution/src/assets/ISO.png",
         "/HackProInfosolution/src/assets/MSME.png",
@@ -24,19 +24,25 @@ const Main = () =>{
             <div className="relative w-full">
                 <img className="w-full h-3/4" src={ image } alt="" />
                 <button className="absolute top-2/3 left-72 bg-slate-50 px-6 py-4 rounded-2xl text-3xl text-violet-900  hover:drop-shadow-xl hover:">Enroll Now</button>
-        </div>
-        <div className="m-20">
-        <div className="flex flex-col">
-            <h3 className="py-10 text-5xl">Courses:</h3>
-            <div>
-                <Card></Card>
-            </div>
-            </div>
-            <h3 className="py-10 text-5xl">Why is Certification Important?</h3>
-            <p className="m-0 p-0 text-3xl">An individual's knowledge, abilities, and expertise in the field of cybersecurity 
+            </div>            
+            <div className="m-20">
+                    <div className="flex flex-col">
+                    <h3 className="py-10 text-5xl">Courses:</h3>
+                    <div className="w-100 flex justify-around">
+                        <Card ctitle="Ethical Hacking & Cyber Security" cdes="Ethical Hacking & Cyber security will teach you the latest commercial-grade hacking tools, techniques and methodologies used by hackers and information security professionals to lawfully hack an organization."/>
+                        <Card ctitle="Certified Penetration Testing" cdes="Delve into understanding how hackers breach operating systems and circumvent antivirus measures. By adopting hackers' strategies and approaches, you'll uncover vulnerabilities within your network. "></Card>
+                        <Card ctitle ="Cyber Forensic Investigation" cdes="Gain in-depth knowledge in the field of Computer forensics & Cyber Crime.Use investigation tools and techniques for analysis of data to identify evidence, Technical Aspects & Legal Aspects related to cyber crime."></Card>
+                        <Card ctitle="Certified Bug Bounty Hunting" cdes="Learn to identify, report, and mitigate software vulnerabilities. Gain recognition and compensation by uncovering security flaws in websites, organizations, and software applications."></Card>
+                    </div>
+                </div>
+            <div className="mt-40">
+                <h3 className="py-10 text-5xl">Why is Certification Important?</h3>
+                <p className="m-0 p-0 text-3xl">An individual's knowledge, abilities, and expertise in the field of cybersecurity 
             are demonstrated in concrete form by the cybersecurity certification program, which is why it is significant.
              It can assist people progress in their careers at their current companies as well as help them differentiate themselves from other applicants on the job market.
              When recruiting or elevating cybersecurity specialists, employers frequently need or favour certificates.</p>
+            </div>
+            
              <div className="my-40">
                 <h3 className="py-20 text-5xl">Tools You Learn -</h3>
                 <div className="flex justify-center">
@@ -49,8 +55,8 @@ const Main = () =>{
                     <Marquee speed={20} autoFill behavior="" direction="left">
                     <div className="flex flex-row ">
                         <div className=" flex items-center">
-                        {row1.map((im) =>(
-                            <img className="h-32 m-10" src={im}></img>
+                            {row1.map((im) =>(
+                                <img className="h-32 m-10" src={im}></img>
                             ))}
                         </div>
                     </div>
@@ -69,4 +75,4 @@ const Main = () =>{
 
     )
 }
-export default Main
+export default Home
