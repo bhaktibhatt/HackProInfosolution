1ac3b8f4bdc69e5b8cf17cf029f8c495592a31c7
Bhakti <bhaktinirbhaybhatt@gmail.com>
2024-04-01 20:07:10 +0530

    editing registration form

--- Client/src/pages/Registerationform.jsx
+++ Client/src/pages/Registerationform.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Registerationform = () => {
   return (
-    <div>Registerationform</div>
+    <div>
+      <h1>Register to get Details</h1>
+      <div>
+        <div className='flex flex-col'>
+          <h3>Personal Details:</h3>
+          <div className='flex'>
+            <input type="text" value={"Full Name"} required/>
+            <input type="email" value={"email"} required/>
+          </div>
+          <div className='flex'>
+            <input type="text" value={"Contact"} required/>
+            <input type="email" value={"Courses"} required/>
+          </div>
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+    </div>
   )
 }
 
