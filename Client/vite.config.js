ac68c9d67356fea89a51fea1c239e509c4972a95
Bhakti <bhaktinirbhaybhatt@gmail.com>
2024-04-06 12:50:40 +0530

    scripts modifiyed outDir added in vite.config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Output directory for built files
+  },
 })
