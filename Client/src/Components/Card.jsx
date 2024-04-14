1211ccb7577c587e31b05c8d313316f3d2c97958
Bhakti <bhaktinirbhaybhatt@gmail.com>
2024-04-14 16:04:46 +0530

    responsive fixes

--- Client/src/Components/Card.jsx
+++ Client/src/Components/Card.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import cardBg from '../assets/card-bg.jpg'
 const card = (props) => {
   return (
-    <div className='w-[270px] lg:w-[300px]  h-[390px] border border-gray-400 rounded-3xl hover:shadow-2xl  hover:scale-105 transition-all'>
+    <div className='w-[270px] lg:w-[300px]  h-[390px] border border-gray-400 rounded-[24px] hover:shadow-2xl  hover:scale-105 transition-all'>
       <div className='relative'>
-        <img className=' rounded-t-3xl w-[100%] h-44 ' src={cardBg} alt="" />
-        <h2 className='p-2 w-full absolute top-12 text-white text-[30px] text-center'>{props.ctitle}</h2>
+        <img className=' rounded-t-[24px] w-[100%] h-[176px] ' src={cardBg} alt="" />
+        <h2 className='p-[8px] w-full absolute lg:top-[48px] top-[24px] text-white text-[30px] text-center'>{props.ctitle}</h2>
       </div>
-      <p className='p-2 mb-0'>{props.cdes}</p>
-      <p className='font-bold p-2 pt-0 text-right'>learn more &gt;&gt;</p>
+      <p className='p-[8px] mb-0'>{props.cdes}</p>
+      <p className='font-bold p-[8px] pt-0 text-right'>learn more &gt;&gt;</p>
     </div>
   )
 }
