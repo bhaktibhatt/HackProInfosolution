6050184adc81bfdbae8fcd5f35a246fd4147b428
Prathmesh Kale <connect.prathmesh905@gmail.com>
2024-04-19 20:09:33 +0530

    added working modules on registration

--- Client/src/Components/ScrollToTop.js
+++ Client/src/Components/ScrollToTop.js
@@ -2,13 +2,13 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-  // Extracts pathname property(key) from an object
-  const { pathname } = useLocation();
+    // Extracts pathname property(key) from an object
+    const { pathname } = useLocation();
 
-  // Automatically scrolls to top whenever pathname changes
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    // Automatically scrolls to top whenever pathname changes
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
 }
 
 export default ScrollToTop;
