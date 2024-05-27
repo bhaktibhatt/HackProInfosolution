74aa2aed69c091240954029d3b53ca4399751990
Bhakti <bhaktinirbhaybhatt@gmail.com>
2024-05-27 23:25:41 +0530

    minor change

--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from "react-router-dom";
 import pageBg from '../assets/pageBg.png'
 const About = () => {
   return (
@@ -12,7 +13,7 @@ const About = () => {
         <div className='mx-[40px] lg:mx-[176px] w-auto text-center lg:text-[24px] space-y-[40px] lg:space-y-[112px]'>
           <p className=''>At HackPro InfoSolutions, we believe that cybersecurity is not just a necessity but a fundamental right in today's digital age. With the increasing prevalence of cyber attacks, it's crucial for individuals and businesses to stay ahead of potential threats. That's where we come in.</p>
           <p className=''>Our team comprises seasoned cybersecurity professionals and ethical hackers who are passionate about sharing their expertise and insights with aspiring cybersecurity enthusiasts. With years of experience in the field, we understand the ever-evolving landscape of cybersecurity and tailor our courses to equip you with practical, real-world skills. Whether you're a beginner looking to start your journey in cybersecurity or a seasoned professional seeking advanced training, we have a wide range of courses to cater to your needs. From ethical hacking and digital forensics to network security and cryptography, our comprehensive curriculum covers various aspects of cybersecurity to ensure you're </p>
-          <p>Check out our courses <span className='text-green'>here</span></p>
+          <p>Register For Courses <span className='text-green'> <NavLink to="/registrationform"> here</NavLink></span></p>
         </div>
         </div>
       </div>
